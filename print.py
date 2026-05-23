a6d597502fa1b4fbf7aa7cb0ee93439cfce80f1d
Angel Thapa <angelthapa4141@gmail.com>
2026-05-23 17:28:53 +0545

    week 3 assignment

---
+++ print.py
@@ -0,0 +1,9 @@
+def rectangle(l, b):
+    area = l * b
+    print("area of rectangle=", area)
+    return area
+
+l = float(input("enter length: "))
+b = float(input("enter breadth: "))
+
+rectangle(l, b)
